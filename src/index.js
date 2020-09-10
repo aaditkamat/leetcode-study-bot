101660336772836fc4cc6a25466b58763a24b001
Aadit Kamat <aadit.k12@gmail.com>
2020-09-10 09:29:16 +0800

    Add LeetCode login button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-require("dotenv").config();
-const { Telegraf } = require("telegraf");
+require('dotenv').config();
+const Telegraf = require('telegraf');
+const Extra = require('telegraf/extra');
+const Markup = require('telegraf/markup');
+
+const createKeyboard = () => {
+  return Markup.inlineKeyboard([
+    Markup.urlButton(
+      'Login to LeetCode',
+      'https://leetcode.com/accounts/login'
+    ),
+  ]);
+};
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) => ctx.reply("Welcome to the LeetCode study bot."));
+
+bot.start(async (ctx) => {
+  await ctx.reply(
+    'Welcome to the LeetCode study bot. To get started, click on the Login button below',
+    Extra.markup(createKeyboard())
+  );
+});
+
 bot.launch();
